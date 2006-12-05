59e99af5c661ca4fe332235cd12bb37de0e19132
Elan Ruusamäe <glen@pld-linux.org>
2006-12-05 07:59:11 +0000

    - fix postimees html
    Changed files:
        plugin/postimees.pm -> 1.4

--- plugin/postimees.pm
+++ plugin/postimees.pm
@@ -7,23 +7,22 @@ my $package = __PACKAGE__;
 $plugin::plugins{$package}++;
 
 
-my $baseurl = "http://www.nali.postimees.ee";
-
+my $baseurl = "http://www.postimees.ee";
 
 sub get_url {
 	my $this = shift;
 
-	my $a = sprintf("%s/%s/naljakulg.php?SECT=koomiks", $baseurl, $this->strftime("%d%m%y"));
+	my $a = sprintf("%s/%s/vabal_ajal/koomiks/index.php", $baseurl, $this->strftime("%d%m%y"));
 	local $_ = $this->fetch_url($a) or return;
 
 	@_ = split /\n+/;
 	my ($url, $desc);
 	while (defined($_ = shift(@_))) {
-		if (m#<tr><td><p class=TextTitle>(.*?)</td></tr>#) {
+		if (m#class="TextRedBig"><b>(.*?)</b></a>#) {
 			$desc = $1;
 		}
 
-		if (m#<td><img src="(/\d+/gfx/[\da-f]+\.\w+)">#) {
+		if (m#<img src="(/foto/\d+/\d+/[\da-f]+\.\w+)" width="\d+" height="\d+" alt=""> </span>#) {
 			$url = $1;
 
 			$this->add_comic("$baseurl$url", $desc);
