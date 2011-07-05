ebc29f6c39f6eda4b1a8f3cb077f9ecc8214a421
Elan Ruusamäe <glen@pld-linux.org>
2011-07-05 18:59:16 +0000

    - unfinished update to use fb wall
    Changed files:
        plugin/pesakond.pm -> 1.3

--- plugin/pesakond.pm
+++ plugin/pesakond.pm
@@ -2,22 +2,42 @@
 package plugin::pesakond;
 
 use plugin;
+use JSON;
+
 push(@ISA, 'plugin');
 my $package = __PACKAGE__;
 $plugin::plugins{$package}++;
 
-my $url = 'http://www.jippii.ee/ee/fun/pesakond/';
+my $graph_url = 'https://graph.facebook.com/118079068203119/feed';
+my $limit = 10;
+
+# access token who can read the wall, you can use yours if you're not afraid
+my $access_token = '';
+
+# uid of wall owner (i.e accept posts only from that uid)
+my $owner = '118079068203119';
 
 sub get_url {
 	my $this = shift;
-	local $_ = $this->fetch_url($url) or return;
 
-	@_ = split /[\r\n]+/;
-	while ($_ = shift(@_)) {
-		next unless m#/ee/fun/koom/#;
-		next unless /<img.+?src="(.+?)".*>/i;
-		return sprintf("http://www.jippii.ee%s", $1);
+	my $url = $graph_url . '&access_token=' . $access_token . '&limit=' . $limit;
+	my $content = $this->fetch_url($url) or return;
+
+	my $json = JSON->new->decode($content);
+	use Data::Dumper;
+	foreach my $data (@{$json->{data}}) {
+		next unless $data->{from}->{id} eq $owner;
+		print "from:", $data->{from}->{id}, "\n";
+
+		next unless defined $data->{name};
+	   	next unless $data->{name} eq 'Wall Photos';
+		print "name:", $data->{name}, "\n";
+
+		print "Link: ", $data->{link}, "\n";
+#		print Dumper \$data;
 	}
+	die;
+
 }
 
 1;
