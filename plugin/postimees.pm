e7561e9f0a53e48785bec5159ec976872e9163f6
Elan Ruusamäe <glen@pld-linux.org>
2011-11-14 18:15:58 +0000

    - adjust for site changes, rewritten using HTML::TreeBuilder
    Changed files:
        plugin/postimees.pm -> 1.9

--- plugin/postimees.pm
+++ plugin/postimees.pm
@@ -3,6 +3,7 @@ package plugin::postimees;
 
 use strict;
 use base 'plugin';
+use HTML::TreeBuilder;
 
 my $package = __PACKAGE__;
 $plugin::plugins{$package}++;
@@ -12,29 +13,31 @@ my $baseurl = 'http://www.postimees.ee';
 sub get_url {
 	my $this = shift;
 
-	my $a = sprintf("%s/?r=202&d=%s", $baseurl, $this->strftime("%Y%m%d"));
-	local $_ = $this->fetch_url($a) or return;
+	my $url = sprintf("%s/?r=202&d=%s", $baseurl, $this->strftime("%Y%m%d"));
+	my $content = $this->fetch_url($url) or return;
 
-	@_ = split /\n+/;
-	my ($url, $desc);
+	my $root = new HTML::TreeBuilder();
+	$root->parse($content);
 
-	while (defined($_ = shift(@_))) {
-		last if /<div class="koomiks_link_vasak">/;
-	}
+	my $c = $root->look_down(_tag => 'div', class => 'sisu_keskmine_paremata') or return;
 
-	while (defined($_ = shift(@_))) {
-		if (m#<a href="\?r=202[^"]+">(\S.+)</a>#) {
-			$desc = $1;
-			next;
+	my @l = $c->look_down(
+		_tag => 'div',
+		sub {
+		   	$_[0]->look_down(_tag => 'div', class => 'koomiks_nimetus')
+			&&
+		   	$_[0]->look_down(_tag => 'div', class => 'koomiks_pilt')
+			&&
+			!$_[0]->look_down(_tag => 'div', class => 'clearer')
+	   	}
+	);
+
+	foreach my $l (@l) {
+		my $t = $l->look_down(_tag => 'div', class => 'koomiks_nimetus')->find('a');
+		my $p = $l->look_down(_tag => 'div', class => 'koomiks_pilt')->find('img');
+		if ($p && $t) {
+			$this->add_comic($p->attr('src'), $t->as_text, $url);
 		}
-
-		if (m#<img.*? src="(http://f.postimees.ee/s/koomiks/.+)".*?/>#) {
-			$url = $1;
-			$this->add_comic($url, $desc);
-			undef $url; undef $desc;
-		}
-
-		last if m#<div class="koomiks_link_vasak">#;
 	}
 }
 
