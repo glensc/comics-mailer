c8d4be3b3e5e92741eff25763c90b51202418eea
Elan Ruusamäe <glen@pld-linux.org>
2012-08-12 13:31:34 +0000

    - store history file in XDG_CONFIG_DIR
    Changed files:
        comics-mailer.pl -> 1.22

--- comics-mailer.pl
+++ comics-mailer.pl
@@ -2,6 +2,9 @@
 use strict;
 use warnings;
 
+use File::BaseDir qw/config_home/;
+
+
 use lib '.';
 use comic;
 use plugin::postimees;
@@ -16,7 +19,9 @@ $debug = 1 if -t STDIN;
 my $p = comic->new();
 
 $p->set_date($date) if $date;
-$p->set_history_file('history.ser');
+# use config_home, as config_files returns only existing files
+my $history_file = config_home('comics-mailer.hist');
+$p->set_history_file($history_file);
 
 $p->fetch_data;
 $p->compose_mail;
