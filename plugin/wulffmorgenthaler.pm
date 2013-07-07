04bee392b45820ade98d92ab3b3c7179e6e4bbb7
Elan Ruusamäe <glen@delfi.ee>
2013-07-07 23:26:05 +0300

    wumo fix by zod

--- plugin/wulffmorgenthaler.pm
+++ plugin/wulffmorgenthaler.pm
@@ -8,7 +8,7 @@ use HTML::TreeBuilder;
 my $package = __PACKAGE__;
 $plugin::plugins{$package}++;
 
-my $baseurl = "http://wulffmorgenthaler.com/";
+my $baseurl = "http://wumo.com/";
 
 sub get_url {
 	my $this = shift;
@@ -16,11 +16,14 @@ sub get_url {
 	my $content = $this->fetch_url($baseurl) or return;
 	my $root = HTML::TreeBuilder->new()->parse($content);
 
-	my $image = $root->look_down(_tag => 'meta', property => 'og:image') or return;
 	my $url = $root->look_down(_tag => 'meta', property => 'og:url') or return;
-	my $title = $root->look_down(_tag => 'meta', property => 'og:title') or return;
 
-	$this->add_comic($image->attr('content'), $title->attr('content'), $url->attr('content'));
+	my $c = $root->look_down(_tag => 'div', class => 'striben') or return;
+	$c = $c->look_down(_tag => 'div', class => 'purple') or return;
+	$c = $c->look_down(_tag => 'div', class => 'inner') or return;
+	$c = $c->look_down(_tag => 'div', class => 'stribe')->find('img');
+
+	$this->add_comic($baseurl.$c->attr('src'), $c->attr('alt'), $url->attr('content'));
 }
 
 1;
