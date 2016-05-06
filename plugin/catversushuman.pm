2f83d40099e38fa144f5ea8d77769949984c7d81
Elan Ruusamäe <glen@delfi.ee>
2016-05-06 10:01:04 +0300

    formatting fix

--- plugin/catversushuman.pm
+++ plugin/catversushuman.pm
@@ -18,19 +18,19 @@ sub get_url {
 
 	my @c = $root->look_down(_tag => 'div', class => 'post-body entry-content') or return;
 
-  foreach my $c (@c) {
-    my $i = $c->look_down(_tag => 'img') or next;
-    my $src = $i->attr('src') or next;
-    $src =~ s/\Qhttps:\E/http:/g;
+	foreach my $c (@c) {
+		my $i = $c->look_down(_tag => 'img') or next;
+		my $src = $i->attr('src') or next;
+		$src =~ s/\Qhttps:\E/http:/g;
 
-    my @t = $c->look_down(_tag => 'div', class => 'separator');
-    my $t = @t[1] && @t[1]->as_text || "catversushuman.com";
+		my @t = $c->look_down(_tag => 'div', class => 'separator');
+		my $t = @t[1] && @t[1]->as_text || "catversushuman.com";
 
-    $this->add_comic($src, $t, $baseurl);
+		$this->add_comic($src, $t, $baseurl);
 
-    # one cat cartoon a day
-    last;
-  }
+		# one cat cartoon a day
+		last;
+	}
 }
 
 1;
