4db4219c39fd6a7889c974abd2f384ba9b6483e2
Elan Ruusamäe <glen@delfi.ee>
2016-11-10 11:13:37 +0200

    avoid overwriting history when debugging
    - use history file only if not debugging
    - do not use cache file if ran from cron

--- comics-mailer.pl
+++ comics-mailer.pl
@@ -30,7 +30,9 @@ my $p = comic->new();
 $p->set_date($date) if $date;
 # use config_home, as config_files returns only existing files
 my $history_file = config_home('comics-mailer.hist');
-$p->set_history_file($history_file);
+# use history file only if not debugging
+$p->set_history_file($history_file) if !$debug;
+# do not use cache file if ran from cron
 $p->set_http_cache(cache_home("comics-mailer-http.cache")) if $debug;
 
 $p->fetch_data;
