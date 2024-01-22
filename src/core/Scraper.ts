3bac2bda3e6fbf3aec3c97a86b2cca72230aabe2
Elan Ruusamäe <glen@delfi.ee>
2024-01-23 01:46:38 +0200

    Add parser api to Scraper

--- src/core/Scraper.ts
+++ src/core/Scraper.ts
@@ -1,7 +1,15 @@
+import { JSDOM } from "jsdom";
+
 export class Scraper {
   public async fetch(url: string) {
     const response = await fetch(url);
 
     return response.text();
+  }
+
+  public createParser(html: string): Document {
+    const dom = new JSDOM(html);
+
+    return dom.window.document;
   }
 }
