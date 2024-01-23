b25d4fc5471e81b2e7dd5ce3ae8a1469cc96e27f
Elan Ruusamäe <glen@delfi.ee>
2024-01-23 09:15:55 +0200

    Update main to use ScrapeRunner service

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
-const main = async (...args: string[]) => {
+import scrapers from "./services/scrapers.ts";
+import mailer from "./services/mailer.ts";
+import scrapeRunner from "./services/scrapeRunner.ts";
+import { MAIL_FROM, MAIL_REPLY_TO } from "./services/config.ts";
 
+const main = async (...recipients: string[]) => {
+  if (!recipients.length) {
+    return;
+  }
+
+  const attachments = await scrapeRunner.run(scrapers);
+
+  await mailer.send({
+    from: MAIL_FROM,
+    date: new Date(),
+    subject: "DAILY: Comics from the Internet ({{date}})",
+    replyTo: MAIL_REPLY_TO,
+    bcc: recipients,
+    attachments,
+  });
 };
 
 if (import.meta.path == Bun.main) {
