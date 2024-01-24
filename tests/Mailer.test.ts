b989e686c44f61ac75107d7981b3ca3ffb900bd6
Elan Ruusamäe <glen@delfi.ee>
2024-01-25 01:41:26 +0200

    Test: Add attachment to mailer test

--- tests/Mailer.test.ts
+++ tests/Mailer.test.ts
@@ -2,6 +2,8 @@ import { Mailer } from "../src/core/Mailer.ts";
 import { MailTransport } from "../src/core/MailTransport.ts";
 import { MailBuilder } from "../src/core/MailBuilder.ts";
 import { VIEWS_PATH } from "../src/services/config.ts";
+import { AttachmentBuilder } from "../src/core/AttachmentBuilder.ts";
+import { HttpClient } from "../src/core/HttpClient.ts";
 
 const test = async () => {
   const mailer = new Mailer(
@@ -11,9 +13,14 @@ const test = async () => {
     }),
   );
 
+  const ab = new AttachmentBuilder(HttpClient.create());
+  const url = "https://wumo.com/img/wumo/2024/01/wumo65aaaaf604acc7.84412774.jpg";
+  const attachment = await ab.create(url, "Wumo 24. Jan 2024", "https://wumo.com/");
+
   const info: any = await mailer.send({
     date: new Date(),
     subject: "DAILY: comics at estonian web ({{date}})",
+    attachments: [attachment],
   });
 
   console.log("envelope", info.envelope);
