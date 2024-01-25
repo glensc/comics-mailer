4c11bbbe452d3375307a6835c687577aade25871
Elan Ruusamäe <glen@delfi.ee>
2024-01-25 20:48:49 +0200

    Register XkcdScraper scraper

--- src/services/scrapers.ts
+++ src/services/scrapers.ts
@@ -1,9 +1,11 @@
 import { OglafScraper } from "../scrapers/OglafScraper.ts";
 import { TurnoffScraper } from "../scrapers/TurnoffScraper.ts";
 import { WumoScraper } from "../scrapers/WumoScraper.ts";
+import { XkcdScraper } from "../scrapers/XkcdScraper.ts";
 
 export default [
   new OglafScraper(),
   new TurnoffScraper(),
   new WumoScraper(),
+  new XkcdScraper(),
 ];
