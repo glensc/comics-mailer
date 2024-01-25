cc06be3ddc717d18210311d47fb22c2fb06dabd9
Elan Ruusamäe <glen@delfi.ee>
2024-01-25 20:43:57 +0200

    Register OglafScraper scraper

--- src/services/scrapers.ts
+++ src/services/scrapers.ts
@@ -1,7 +1,9 @@
+import { OglafScraper } from "../scrapers/OglafScraper.ts";
 import { TurnoffScraper } from "../scrapers/TurnoffScraper.ts";
 import { WumoScraper } from "../scrapers/WumoScraper.ts";
 
 export default [
+  new OglafScraper(),
   new TurnoffScraper(),
   new WumoScraper(),
 ];
