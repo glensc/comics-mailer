d7540d91178b833123c9b1e46b65ddbd3a98ff00
Elan Ruusamäe <glen@delfi.ee>
2024-01-25 16:42:19 +0200

    Register TurnoffScraper scraper

--- src/services/scrapers.ts
+++ src/services/scrapers.ts
@@ -1,5 +1,7 @@
+import { TurnoffScraper } from "../scrapers/TurnoffScraper.ts";
 import { WumoScraper } from "../scrapers/WumoScraper.ts";
 
 export default [
+  new TurnoffScraper(),
   new WumoScraper(),
 ];
