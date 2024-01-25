80e2c687ed69c666b3bcd9389e6645d50391f21e
Elan Ruusamäe <glen@delfi.ee>
2024-01-25 19:10:53 +0200

    Update HttpClient to use NodeFetchCache

--- src/core/HttpClient.ts
+++ src/core/HttpClient.ts
@@ -1,13 +1,30 @@
+import NodeFetchCache, { FileSystemCache, MemoryCache } from "node-fetch-cache";
+import type { NodeFetch } from "./types.ts";
+
+type HttpClientOptions = {
+  cacheDirectory?: string;
+};
+
 export class HttpClient {
-  public static create(): HttpClient {
-    return new HttpClient();
+  public constructor(
+    private readonly client: NodeFetch,
+  ) {
+  }
+
+  public static create({ cacheDirectory }: HttpClientOptions = {}): HttpClient {
+    const cache = cacheDirectory ? new FileSystemCache({
+      // Specify where to keep the cache. If undefined, '.cache' is used by default.
+      // If this directory does not exist, it will be created.
+      cacheDirectory,
+    }) : new MemoryCache();
+
+    return new HttpClient(
+      NodeFetchCache.create({
+        cache,
+      }));
   }
 
   public async fetch(url: string) {
-    try {
-      return await fetch(url);
-    } catch (e) {
-      throw new Error(`${e}: ${JSON.stringify(e)}`);
-    }
+    return this.client(url);
   }
 }
