36934c09cf2bf27a6a6090d5eccfa91a0743f273
Elan Ruusamäe <glen@delfi.ee>
2024-02-01 22:28:19 +0200

    Linter: Use semicolons for type items

--- src/core/MailOptions.ts
+++ src/core/MailOptions.ts
@@ -1,12 +1,12 @@
 import type { Attachment } from "nodemailer/lib/mailer";
 
 export type MailOptions = {
-  date?: Date,
-  from?: string,
-  replyTo?: string,
-  bcc?: string[],
-  subject: string,
-  timezone?: string,
+  date?: Date;
+  from?: string;
+  replyTo?: string;
+  bcc?: string[];
+  subject: string;
+  timezone?: string;
   context?: any;
   attachments?: Attachment[];
 }
