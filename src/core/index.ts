b5fe84e056266432e1cab02558a9b937d01816ba
Elan Ruusamäe <glen@delfi.ee>
2024-02-01 22:59:24 +0200

    Add ScrapeRunner to core export

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,6 +3,7 @@ export { HttpClient } from "./HttpClient";
 export { MailBuilder } from "./MailBuilder";
 export { MailTransport } from "./MailTransport";
 export { Mailer } from "./Mailer";
+export { ScrapeRunner } from "./ScrapeRunner";
 export { Scraper } from "./Scraper";
 export type { MailOptions } from "./MailOptions";
 export type { ScraperInterface } from "./ScraperInterface";
