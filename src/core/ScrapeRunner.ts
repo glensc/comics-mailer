8e330026d5b2529ebdf7c66e94fc3205704366dd
Elan Ruusamäe <glen@delfi.ee>
2024-02-01 23:17:37 +0200

    Add ScrapeRunner method and handle attachments download

--- src/core/ScrapeRunner.ts
+++ src/core/ScrapeRunner.ts
@@ -1,13 +1,32 @@
+import type { AttachmentBuilder } from "./AttachmentBuilder.ts";
 import type { ScraperInterface } from "./ScraperInterface.ts";
 import type { Scraper } from "./Scraper.ts";
 
 export class ScrapeRunner {
   public constructor(
     private readonly scraper: Scraper,
+    private readonly ab: AttachmentBuilder,
   ) {
   }
 
   public async run(scrapers: ScraperInterface[]) {
+    const scraped = await this.scrape(scrapers);
+    const attachments = await this.getAttachments(scraped);
+
+    return attachments;
+  }
+
+  private async getAttachments(scraped: string[][]) {
+    return Promise.all(Array.from(this.prepareAttachments(scraped)));
+  }
+
+  private* prepareAttachments(scraped: string[][]) {
+    for (const [url, description, baseUrl] of scraped) {
+      yield this.ab.create(url, description, baseUrl);
+    }
+  }
+
+  private async scrape(scrapers: ScraperInterface[]) {
     const collected = [];
     const promises = Promise.all(Array.from(this.prepare(scrapers)));
     for await (const result of await promises) {
