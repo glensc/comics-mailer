cb05cb92cb16139c32047d860e10fa90f6913f25
Elan Ruusamäe <glen@delfi.ee>
2024-02-01 12:31:10 +0200

    Add GMAIL_USERNAME, GMAIL_PASSWORD to config

--- src/services/config.ts
+++ src/services/config.ts
@@ -2,4 +2,7 @@ import { join } from "path";
 
 const env = process.env;
 
+export const GMAIL_USERNAME = env.GMAIL_USERNAME || "";
+export const GMAIL_PASSWORD = env.GMAIL_PASSWORD || "";
+
 export const VIEWS_PATH = join(__dirname, "../views");
