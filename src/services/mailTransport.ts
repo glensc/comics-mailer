842446b66e8d26a72e25ad2970ef29383f359db2
Elan Ruusamäe <glen@delfi.ee>
2025-01-07 19:07:35 +0200

    Cleanup: Remove un-needed any cast

--- src/services/mailTransport.ts
+++ src/services/mailTransport.ts
@@ -1,7 +1,7 @@
 import { MailTransport } from "../core";
 import { GMAIL_PASSWORD, GMAIL_USERNAME } from "./config.ts";
 
-const options: any = {
+const options = {
   service: "gmail",
   auth: {
     user: GMAIL_USERNAME,
